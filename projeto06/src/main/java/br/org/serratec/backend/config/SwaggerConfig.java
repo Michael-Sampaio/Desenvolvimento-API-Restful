6fefd2010a35b887f0afc97e0a28901aec059a58
Michael Sampaio <tradsampaio@gmail.com>
2021-11-04 15:27:49 -0300

    aplicação do swagger

--- projeto06/src/main/java/br/org/serratec/backend/config/SwaggerConfig.java
+++ projeto06/src/main/java/br/org/serratec/backend/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package br.org.serratec.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -15,9 +18,22 @@ public class SwaggerConfig {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
             .select()
-            .apis(RequestHandlerSelectors.any())
+            .apis(RequestHandlerSelectors.basePackage("br.org.serratec.backend.controller"))
             .paths(PathSelectors.any())
+            .build().apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        ApiInfo apiInfo = new ApiInfoBuilder()
+            .title("API DE MANUTENCAO DE VEICULOS")
+            .description("Essa é uma API de manutencão de veículos")
+            .license("Apache License version 2.0")
+            .licenseUrl("https://www.apache.org/license")
+            .version("1.0.0")
+            .contact(new Contact("Serratec", "www.serratec.org.br", "serratec@gmail.com"))
             .build();
+        return apiInfo;
+
     }
     
 }
--- projeto06/src/main/java/br/org/serratec/backend/controller/VeiculoController.java
+++ projeto06/src/main/java/br/org/serratec/backend/controller/VeiculoController.java
@@ -20,6 +20,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import br.org.serratec.backend.model.Veiculo;
 import br.org.serratec.backend.repository.VeiculoRepository;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/veiculos")
@@ -29,10 +32,26 @@ public class VeiculoController {
     private VeiculoRepository veiculoRepository;
 
     @GetMapping
+    @ApiOperation(value = "Listar todos os veículos", notes = "Listagem de veículos")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Retorna todos os veículos"),
+        @ApiResponse(code = 401, message = "Erro de autenticação"),
+        @ApiResponse(code = 403, message = "Recurso proibido"),
+        @ApiResponse(code = 404, message = "Recurso não encontrado"),
+        @ApiResponse(code = 500, message = "Erro do servidor")
+    })
     public List<Veiculo> listar() {
         return veiculoRepository.findAll();
     }
 
+    @ApiOperation(value = "Buscar um veículo", notes = "Busca de veículos")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Veículo encontrado"),
+        @ApiResponse(code = 401, message = "Erro de autenticação"),
+        @ApiResponse(code = 403, message = "Recurso proibido"),
+        @ApiResponse(code = 404, message = "Recurso não encontrado"),
+        @ApiResponse(code = 500, message = "Erro do servidor")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Veiculo> buscar(@PathVariable Long id) {
         Optional<Veiculo> veiculo = veiculoRepository.findById(id);
@@ -42,6 +61,14 @@ public class VeiculoController {
         return ResponseEntity.ok(veiculo.get());
     }
 
+    @ApiOperation(value = "Cadastrar um veículo", notes = "Cadastro de veículos")
+    @ApiResponses(value = {
+        @ApiResponse(code = 201, message = "Cadastra veículo"),
+        @ApiResponse(code = 401, message = "Erro de autenticação"),
+        @ApiResponse(code = 403, message = "Recurso proibido"),
+        @ApiResponse(code = 404, message = "Recurso não encontrado"),
+        @ApiResponse(code = 500, message = "Erro do servidor")
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Veiculo inserir(@Valid @RequestBody Veiculo veiculo) {
